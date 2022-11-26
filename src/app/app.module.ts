859f417038a92eec13b84159f8842f540e786206
ext-andrei-cinc <ext_andrei.cinc@btrl.ro>
2022-11-26 20:11:51 +0200

    Create the full screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ContentComponent } from './components/content/content.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
